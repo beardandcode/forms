568c68e9ec71485c18a2874c97fda090912934c5
Tom Booth <tombooth@gmail.com>
2016-06-18 23:14:08 +0100

    Release 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.beardandcode/forms "0.1.3"
+(defproject com.beardandcode/forms "0.1.4"
   :description "A library to generate and validate HTML forms based on JSON Schema"
   :url "https://github.com/beardandcode/forms"
   :license {:name "MIT"
